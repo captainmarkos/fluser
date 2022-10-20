acba69d79c4523eb9f6b64d5714fdbe24084eebe
Mark Brettin <mark.brettin@rever.co>
2022-10-20 17:42:12 -0600

    add route for /users and /login

--- api/auth.py
+++ api/auth.py
@@ -23,6 +23,9 @@ def load_logged_in_user():
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    if request.method != 'POST':
+        return { 'error': 'POST method required' }
+
     email = request.json['email']
     password = request.json['password']
     db = get_db()
@@ -43,4 +46,46 @@ def register():
         return { 'error': "Email '" + email + "' is already registered." }
 
     return { 'success': "Email '" + email + "' successfully registered." }
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method != 'POST':
+        return { 'error': 'POST method required' }
+
+    email = request.json['email']
+    password = request.json['password']
+    db = get_db()
+
+    user = db.execute(
+        'SELECT * FROM users WHERE email = ?', (email,)
+    ).fetchone()
+
+    if user is None:
+        return { 'error': 'Incorrect email ' + email }
+    elif not check_password_hash(user['password'], password):
+        return { 'error': 'Incorrect password' }
+
+    return { 'success': 'User authenticated' }
+
+@bp.route('/users', methods=('GET', 'POST'))
+def users():
+    if request.method == 'POST':
+        return { 'error': 'GET method required' }
+
+    db = get_db()
+
+    users = db.execute('SELECT * FROM users').fetchall()
+
+    if users is None:
+        return { 'count': 0, 'error': 'No users registered' }
+
+    #for user in users:
+    user_list = []
+    for user in users:
+      user_list.append({
+        'email': user['email'],
+        'created_at': user['created_at']
+      })
+
+    return { 'users': user_list, 'count': len(users), 'success': 'Users fetched' }
 
